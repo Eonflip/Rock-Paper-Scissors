d78f6c9f122200a248a31c18114cd2f98b2abe8b
Ian Jones <ianj1993@gmail.com>
2022-02-01 21:00:17 -0700

    Print with DOM

--- rps.js
+++ rps.js
@@ -4,7 +4,98 @@ let playerSelection;
 let computerSelection;
 
 
-function computerPlay(){
+const body = document.querySelector('body');
+
+const rock = document.createElement('button');
+
+rock.classList.add('1');
+
+rock.textContent = 'Rock';
+
+body.appendChild(rock);
+
+const paper = document.createElement('button');
+
+paper.classList.add('2');
+
+paper.textContent = 'Paper';
+
+body.appendChild(paper);
+
+const scissors = document.createElement('button');
+
+scissors.classList.add('3');
+
+scissors.textContent = 'Scissors';
+
+body.appendChild(scissors);
+
+const resultsDisplay = document.createElement('div');
+
+resultsDisplay.classList.add('results');
+
+const results = document.createElement('p');
+
+body.appendChild(resultsDisplay);
+
+const winCondition = () => {
+    if (playerScore == 5){
+        const resultsPrint = document.createElement('div');
+        resultsPrint.classList.add('print');
+        resultsPrint.textContent = "You win, congratulations. The machines can't take over while you're around!";
+        body.appendChild(resultsPrint);
+    } else if (computerScore == 5){
+        const resultsPrint = document.createElement('div');
+        resultsPrint.classList.add('print');
+        resultsPrint.textContent = "You lose, oh no the machine uprising has begun!";
+        body.appendChild(resultsPrint);
+    }
+}
+
+
+
+
+rock.addEventListener('click', () => {
+    playerSelection = 'ROCK';
+    gamePlay(playerSelection, computerSelection);
+    if (playerScore == 3) {
+        console.log("You win, congratulations. The machines can't take over while you're around!");
+    } else if (computerScore == 3) {
+        console.log("You lose, oh no the machine uprising has begun!");
+    }
+    winCondition();
+    return;
+});
+
+paper.addEventListener('click', () => {
+    let playerScore = 0;
+    playerSelection = 'PAPER';
+    gamePlay(playerSelection, computerSelection);
+    if (playerScore == 3) {
+        console.log("You win, congratulations. The machines can't take over while you're around!");
+    } else if (computerScore == 3) {
+        console.log("You lose, oh no the machine uprising has begun!");
+    }
+    winCondition();
+    return;
+});
+
+scissors.addEventListener('click', () => {
+    let playerScore = 0;
+    playerSelection = 'SCISSORS';
+    gamePlay(playerSelection, computerSelection);
+    if (playerScore == 3) {
+        console.log("You win, congratulations. The machines can't take over while you're around!");
+    } else if (computerScore == 3) {
+        console.log("You lose, oh no the machine uprising has begun!");
+    }
+    winCondition();
+    return;
+});
+
+console.log(playerSelection);
+
+const computerPlay = () => {
     choice = Math.random() * 100;
 
     if (choice >= 67) {
@@ -16,60 +107,71 @@ function computerPlay(){
     else  {
         return "Paper";
     }
-}
+};
 
-
-function gamePlay(playerSelection, computerSelection){
-
-        playerSelection = prompt("Select Rock, Paper, or Scissors").toUpperCase();
+const gamePlay = (playerSelection, computerSelection) => {
 
         computerSelection = computerPlay().toUpperCase();
 
         if (playerSelection == computerSelection){
-            console.log("Its a draw!");
-        }
-        else if (playerSelection == "ROCK"){
+                const drawPrint = document.createElement('div');
+                drawPrint.classList.add('drawPrint');
+                drawPrint.textContent = "It's a draw try again!";
+                body.appendChild(drawPrint);
+        } else if (playerSelection == "ROCK"){
             if (computerSelection == "SCISSORS"){
-                console.log("Rock beats scissors, you win!");
+                const winPrint = document.createElement('div');
+                winPrint.classList.add('winPrint');
+                winPrint.textContent = "Rock beats scissors, you win!";
+                body.appendChild(winPrint);
                 playerScore++;
-            }
-            else {
-                console.log("Paper beats rock, you lose.");
+            } else {
+                const losePrint = document.createElement('div');
+                losePrint.classList.add('losePrint');
+                losePrint.textContent = "Paper beats rock, you lose!";
+                body.appendChild(losePrint);
                 computerScore++;
             }
-        }
-        else if (playerSelection == "SCISSORS"){
+        } else if (playerSelection == "SCISSORS"){
             if (computerSelection == "PAPER"){
-                console.log("Scissors beats paper, you win!");
+                const winPrint = document.createElement('div');
+                winPrint.classList.add('winPrint');
+                winPrint.textContent = "Scissors beats paper, you win!";
+                body.appendChild(winPrint);
                 playerScore++;
-            }
-            else {
-                console.log("Rock beats scissors, you lose.");
+            } else {
+                const losePrint = document.createElement('div');
+                losePrint.classList.add('losePrint');
+                losePrint.textContent = "Rock beats scissors, you lose!";
+                body.appendChild(losePrint);
                 computerScore++;
             }
-        }
-        else if (playerSelection == "PAPER"){
+        } else if (playerSelection == "PAPER"){
             if (computerSelection ==  "ROCK"){
-                console.log("Paper beats rock, you win!");
+                const winPrint = document.createElement('div');
+                winPrint.classList.add('winPrint');
+                winPrint.textContent = "Paper beats rock, you win!";
+                body.appendChild(winPrint);
                 playerScore++;
-            }
-            else{
-                console.log("Scissors beats paper, you lose.");
+            } else{
+                const losePrint = document.createElement('div');
+                losePrint.classList.add('losePrint');
+                losePrint.textContent = "Scissors beats paper, you lose!";
+                body.appendChild(losePrint);
                 computerScore++;
             }
 
         }
+
+        results.innerHTML = `Your score is ${playerScore} the computer's score is ${computerScore}`;
+
+        resultsDisplay.appendChild(results);
+    };
+
+    if (playerScore == 3) {
+        console.log("You win, congratulations. The machines can't take over while you're around!");
+    } else if (computerScore == 3) {
+        console.log("You lose, oh no the machine uprising has begun!");
     }
 
 
-function game() {
-    while (playerScore < 3 && computerScore < 3) {
-        gamePlay(playerSelection, computerSelection);
-    } if (playerScore === 2) {
-        console.log("Congratulations, you win!");
-    } else if (computerScore === 2) {
-        console.log("Wow you got beat by a computer, embarrassing");
-    }
-}
-
-game();
